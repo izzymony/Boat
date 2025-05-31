99f6eb68d8a17d17fecb6884589918998402e7ac
izzymony <ojehonmonisrael008@gmail.com>
2025-06-01 00:04:25 +0100

    added some menu bar

--- contact.js
+++ contact.js
@@ -3,6 +3,70 @@ const nameInput = document.getElementById('name');
 const emailInput = document.getElementById('email');
 const messageInput = document.getElementById('message');
 const surnameInput = document.getElementById('surname')
+const menuItems =[
+  {text:'Home', href: '#' , type:'Home'},
+  {text:'Help' , href: '#', type:'Help'},
+  {text:'Profile', href:'#', type:'Profile'},  
+  {text:'Booking', href:'#', type:'Booking'},  
+  {text:'History', href:'#', type:'History'} 
+]
+
+const menu = document.getElementById('menu');
+const menuContainer = document.getElementById('menu-container');
+const toggleMenu = document.getElementById('toggle-menu');
+const closeMenu = document.getElementById('close-menu');
+
+menuItems.forEach((item) =>{
+ const menuItem = document.createElement('li')
+ const link = document.createElement('a');
+ link.href = item.text;
+ link.textContent = item.text;
+ link.classList.add ('block' ,'py-6' ,'text-black', 'hover:bg-gray-100', 'font-medium');
+
+ if(item.text === 'Home'){
+  link.addEventListener('click', (e) =>{
+    preventDefault(e);
+    window.location = 'index.html'
+  })
+ }
+ if(item.text === 'Help'){
+  link.addEventListener('click', (e) =>{
+    preventDefault(e);
+    window.location = 'Help.html'
+  })
+ }
+ if(item.text === 'Profile'){
+  link.addEventListener('click', (e) =>{
+    preventDefault(e);
+    window.location = 'Profile.html'
+  })
+ }
+ if(item.text === 'Booking'){
+  link.addEventListener('click', (e) =>{
+    preventDefault(e);
+    window.location = 'Booking.html'
+  })
+ }
+
+ if(item.text === 'History'){
+  link.addEventListener('click', (e) => {
+    preventDefault(e);
+    window.location = 'history.html'
+  })
+ }
+
+ menuItem.appendChild(link)
+ menu.appendChild(menuItem)
+ 
+})
+
+toggleMenu.addEventListener('click' , (e) =>{
+  menuContainer.classList.toggle('hidden')
+})
+
+closeMenu.addEventListener('click' ,(e) =>{
+  menuContainer.classList.toggle('hidden')
+} )
 
 
 form.addEventListener('submit', (e) => {
